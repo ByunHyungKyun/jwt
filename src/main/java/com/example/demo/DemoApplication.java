4a09f16ea6a95badeacfd3ba92c52daf69e75daa
byun <princebyun@gmail.com>
2024-02-02 14:41:41 +0900

    jwt 연습

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package princebyun;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class JwtApplication {
-
+public class DemoApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(JwtApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
